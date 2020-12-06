c30c1be68df90b004fce3d0e68e6ff4080c81ac1
honda46 <honda@star.rcast.u-tokyo.ac.jp>
2020-12-06 10:18:35 +0900

    third commit

--- app/components/QR_display.js
+++ app/components/QR_display.js
@@ -8,7 +8,7 @@ const Sample = () => {
     const db = firebase.firestore();
     const doc = await db.collection("body_temperature").doc("4d84baLgsEO9Artf8EDxTOwt60t2").collection("date").doc("20201205").get();
     const result = JSON.stringify(doc.data());
-    const temp = result.replace( "{\"vtext\":\"" , "" ) ;
+    //const temp = result.replace( "{\"vtext\":\"" , "" ) ;
     //const temp2 = "temp" + temp;
     setValue(result);
   }

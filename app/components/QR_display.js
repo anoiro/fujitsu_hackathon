559529ef6803669e155854b6bc6a2423a3da1172
honda46 <honda@star.rcast.u-tokyo.ac.jp>
2020-12-06 11:04:30 +0900

    fix_QR

--- app/components/QR_display.js
+++ app/components/QR_display.js
@@ -1,45 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, {Component, useState, useEffect } from "react";
 import firebase from "firebase";
 import { firestore,  auth } from "../store";
 import "firebase/storage";
 import QRCode from "qrcode.react";
 
-const QR_display = () => {
-	var today, tmp, day;
-	today = new Date();
-	if (today.getDate() < 10) {
-		day = '0' + String(today.getDate());
-	} else {
-		day = String(today.getDate());
-	}
-	tmp = String(today.getFullYear()) + String(today.getMonth()+1) + String(day);
-	var uid;
-	//auth.onAuthStateChanged((user) => {
-	//	if (user) {
-	//		console.log(user);
-	//		console.log(user.uid);
-	//		uid = user.uid;
-	//	} else {
-	//		this.setState({login: false});
-	//	}
-	//});
-  const [value, setValue] = useState("");
-  async function getFireData() {
+
+class QR_display extends Component {
+	// var today, tmp, day;
+	// today = new Date();
+	// if (today.getDate() < 10) {
+	// 	day = '0' + String(today.getDate());
+	// } else {
+	// 	day = String(today.getDate());
+	// }
+	// tmp = String(today.getFullYear()) + String(today.getMonth()+1) + String(day);
+	// var uid;
+	// auth.onAuthStateChanged((user) => {
+	// 	if (user) {
+	// 		console.log(user);
+	// 		console.log(user.uid);
+	// 		uid = user.uid;
+	// 	} else {
+	// 		this.setState({login: false});
+	// 	}
+	// });
+	constructor(props) {
+    super(props);
+    this.state = {data: 'No data'};
+  }
+
+  handClickFetchButton = async () => {
     const db = firestore;
-    const doc = await db.collection("body_temperature").doc("PPYnpLExZuXof2G0IqPqFGwQ9u33").collection("date").doc(tmp).get();
+    const doc = await db.collection("body_temperature").doc("PPYnpLExZuXof2G0IqPqFGwQ9u33").collection("date").doc("1207").get();
     //const doc = await db.collection("body_temperature").doc(uid).collection("date").doc(tmp).get();
     const result = JSON.stringify(doc.data());
     //const temp = result.replace( "{\"vtext\":\"" , "" ) ;
     //const temp2 = "temp" + temp;
-    setValue(result);
+		this.setState({data: result});
   }
-  useEffect(() => {
-    getFireData();
-  }, []);
-  return (
-    <div className="Sample">
-      {value ? <QRCode value={value} /> : "...loading"}
-    </div>
-  );
+
+	render(){
+		return (
+	    <div className="Sample">
+			<button onClick={this.handClickFetchButton}>表示</button>
+			<br></br>
+			<br></br>
+	      {<QRCode value={this.state.data} /> }
+	    </div>
+	  );
+  }
+
 };
+
 export default QR_display;

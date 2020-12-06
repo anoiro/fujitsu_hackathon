4727d4f8e7c997d4d93b7b444f4a9ca77294070a
baskei0130 <keita.shimada.abc@keio.jp>
2020-12-06 11:40:19 +0900

    Done qr_display

--- app/components/QR_display.js
+++ app/components/QR_display.js
@@ -25,18 +25,58 @@ class QR_display extends Component {
 	// 	}
 	// });
 	constructor(props) {
-    super(props);
-    this.state = {data: 'No data'};
-  }
+		super(props);
+		this.state = {
+			email: '',
+			pass: '',
+			created: '',
+			message: 'mail，パスワードを入力してください．パスワードは6文字以上でお願いします．',
+			signup: false,
+			uid: '',
+			login: false,
+    	data: 'No data'
+		}
+	}
+	componentDidMount() {
+		auth.onAuthStateChanged((user) => {
+			if (user) {
+				console.log(user);
+				console.log(user.uid);
+				this.setState({uid: user.uid, login: true});
+			} else {
+				this.setState({login: false});
+			}
+		});
+		console.log(this.state.uid);
+	}
 
   handClickFetchButton = async () => {
+		await auth.onAuthStateChanged((user) => {
+			if (user) {
+				console.log(user);
+				console.log(user.uid);
+				this.setState({uid: user.uid, login: true});
+			} else {
+				this.setState({login: false});
+			}
+		});
+		console.log(this.state.uid);
+	 	var today, tmp, day;
+	 	today = new Date();
+	 	if (today.getDate() < 10) {
+	 		day = '0' + String(today.getDate());
+	 	} else {
+	 		day = String(today.getDate());
+	 	}
+	 	tmp = String(today.getFullYear()) + String(today.getMonth()+1) + String(day);
     const db = firestore;
-    const doc = await db.collection("body_temperature").doc("PPYnpLExZuXof2G0IqPqFGwQ9u33").collection("date").doc("1207").get();
-    //const doc = await db.collection("body_temperature").doc(uid).collection("date").doc(tmp).get();
-    const result = JSON.stringify(doc.data());
-    //const temp = result.replace( "{\"vtext\":\"" , "" ) ;
-    //const temp2 = "temp" + temp;
+   	//const doc = await db.collection("body_temperature").doc("PPYnpLExZuXof2G0IqPqFGwQ9u33").collection("date").doc("1207").get();
+   	const doc = await db.collection("body_temperature").doc(this.state.uid).collection("date").doc(tmp).get();
+   	const result = JSON.stringify(doc.data());
+   	//const temp = result.replace( "{\"vtext\":\"" , "" ) ;
+   	//const temp2 = "temp" + temp;
 		this.setState({data: result});
+		console.log(this.state.uid);
   }
 
 	render(){

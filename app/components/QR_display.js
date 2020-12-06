33e18a0c056e8a21d8b8b52f2381f154fb737156
baskei0130 <keita.shimada.abc@keio.jp>
2020-12-06 12:19:19 +0900

    done qr_display

--- app/components/QR_display.js
+++ app/components/QR_display.js
@@ -6,24 +6,6 @@ import QRCode from "qrcode.react";
 
 
 class QR_display extends Component {
-	// var today, tmp, day;
-	// today = new Date();
-	// if (today.getDate() < 10) {
-	// 	day = '0' + String(today.getDate());
-	// } else {
-	// 	day = String(today.getDate());
-	// }
-	// tmp = String(today.getFullYear()) + String(today.getMonth()+1) + String(day);
-	// var uid;
-	// auth.onAuthStateChanged((user) => {
-	// 	if (user) {
-	// 		console.log(user);
-	// 		console.log(user.uid);
-	// 		uid = user.uid;
-	// 	} else {
-	// 		this.setState({login: false});
-	// 	}
-	// });
 	constructor(props) {
 		super(props);
 		this.state = {
@@ -34,7 +16,8 @@ class QR_display extends Component {
 			signup: false,
 			uid: '',
 			login: false,
-    	data: 'No data'
+    	data: '',
+			warning: '取得ボタンを押してください'
 		}
 	}
 	componentDidMount() {
@@ -74,9 +57,21 @@ class QR_display extends Component {
    	const doc = await db.collection("body_temperature").doc(this.state.uid).collection("date").doc(tmp).get();
 		console.log(doc.data());
    	const result = JSON.stringify(doc.data());
+		let temp = 0;
    	//const temp = result.replace( "{\"vtext\":\"" , "" ) ;
    	//const temp2 = "temp" + temp;
 		this.setState({data: result});
+		for(let i = 0; i < result.length; i++){
+			if(result[i] == 'p'){
+				temp = result[i + 3] + result[i + 4] + result[i + 6];
+			}
+		}
+		const tn = parseFloat(temp / 10);
+		if(tn >= 37.5){
+			this.setState({warning: '体温が37.5度を超えています。'})
+		}else{
+			this.setState({warning: '正常な体温です'})
+		}
 		console.log(this.state.uid);
   }
 
@@ -86,7 +81,7 @@ class QR_display extends Component {
 			<button onClick={this.handClickFetchButton}>表示</button>
 			<br></br>
 			<br></br>
-	      {<QRCode value={this.state.data} /> }
+			{this.state.data ? <QRCode value={this.state.data} /> : "...loading"}
 	    </div>
 	  );
   }
